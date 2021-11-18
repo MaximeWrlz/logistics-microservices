71383547646923080a2b386a5abc60844db118cb
Maxime <maxime.warluzel@ynov.com>
2021-11-18 17:40:12 +0100

    refacto and add catalog existing products verifications

--- apps/referential/src/app/stock/stock.controller.ts
+++ apps/referential/src/app/stock/stock.controller.ts
@@ -1,7 +1,7 @@
 import { Stock } from '../entities/stock';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, NotFoundException, Param, Post } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Document, Model, Types } from 'mongoose';
 import { StockMovementDto, StockProductDto } from '@log/contracts';
 import { lastValueFrom, map } from 'rxjs';
 import { HttpService } from '@nestjs/axios';
@@ -20,54 +20,62 @@ export class StockController {
   @Post(":id/movement")
   public async updateStock(@Param('id') id: string, @Body() stockMovement: StockMovementDto) {
     const existingStock = await this.stockModel.findOne({productId: id}).exec();
-    if (existingStock && stockMovement.status === 'Supply') {   
-      existingStock.available += stockMovement.quantity;
-      await existingStock.save();      
-    } else if (existingStock && stockMovement.status === 'Reserve') {
-      if (stockMovement.quantity > existingStock.available) {
-        return {
-          "status_code": 400,
-          "message": "Il n'y a plus assez de stock sur ce produit !",
-          "data": existingStock.available + " produits disponibles restants."
-        };
-      } else {
-        existingStock.available -= stockMovement.quantity;
-        existingStock.reserved += stockMovement.quantity;
-        await existingStock.save();
-        if (existingStock.available == 0) {
-          const res = await lastValueFrom(this.http.get(`https://fhemery-logistics.herokuapp.com/api/products/${existingStock.productId}`).pipe(
-            map(resp => resp.data)
-          ));
-          const data = {
-            ean: res.ean
-          }
-          this.http.post(`https://fhemery-logistics.herokuapp.com/api/supply-request`, data);
-        }
-        return {
-          "status_code": 204,
-          "message": "Produits réservés.",
-          "data": existingStock.available + " produits disponibles restants"
-        };
-      }
-    } else if (existingStock && stockMovement.status === 'Removal') {
-      if (stockMovement.quantity > existingStock.reserved) {
-        return {
-          "status_code": 400,
-          "message": "Le stock de produits reservés n'est pas suffisant !",
-          "data": existingStock.reserved + " produits reservés restants."
-        };
-      } else {
-        existingStock.reserved -= stockMovement.quantity;
-        await existingStock.save();
-        return {
-          "status_code": 204,
-          "message": "Produits envoyé à la livraison.",
-        };
-      }
-    } else { 
-      console.log('Did not find the product, adding it', stockMovement);
+    switch (stockMovement.status) {
+      case "Supply":
+        return this.Supply(id, existingStock, stockMovement);
+      case "Reserve":
+        return this.Reserve(existingStock, stockMovement);
+      case "Removal":
+        return this.Remove(existingStock, stockMovement);    
+      default:
+        break;
+    }
+  }
+
+  public async Supply(id: string, existingStock: Document<unknown, unknown, Stock> & Stock & { _id: Types.ObjectId; }, stockMovement: StockMovementDto) {
+    const existInCatalog = await lastValueFrom(this.http.get(`https://fhemery-logistics.herokuapp.com/api/products/${existingStock.productId}`).pipe(
+      map(resp => resp.data)
+    ));
+    if (!existInCatalog) {
+      return new HttpException("Le produit n'est pas répertorié dans le catalogue !", 400);
+    }
+    if (!existingStock) {
       const newObject = new this.stockModel({productId: id, available: stockMovement.quantity, reserved: 0} as Stock)
       await newObject.save();
+      return 204;
+    }
+    existingStock.available += stockMovement.quantity;
+    await existingStock.save();
+    return 204;
+  }
+
+  public async Reserve(existingStock: Document<unknown, unknown, Stock> & Stock & { _id: Types.ObjectId; }, stockMovement: StockMovementDto) {
+    if (stockMovement.quantity > existingStock.available) {
+      return new HttpException(`Il n'y a plus assez de stock sur ce produit ! (${existingStock.available} produits disponibles restants)`, 400);
+    } else {
+      existingStock.available -= stockMovement.quantity;
+      existingStock.reserved += stockMovement.quantity;
+      await existingStock.save();
+      if (existingStock.available == 0) {
+        const res = await lastValueFrom(this.http.get(`https://fhemery-logistics.herokuapp.com/api/products/${existingStock.productId}`).pipe(
+          map(resp => resp.data)
+        ));
+        const data = {
+          ean: res.ean
+        }
+        this.http.post(`https://fhemery-logistics.herokuapp.com/api/supply-request`, data);
+      }
+      return 204;
+    }
+  }
+
+  public async Remove(existingStock: Document<unknown, unknown, Stock> & Stock & { _id: Types.ObjectId; }, stockMovement: StockMovementDto) {
+    if (stockMovement.quantity > existingStock.reserved) {
+      return new HttpException(`Le stock de produits reservés n'est pas suffisant ! (${existingStock.reserved} produits reservés restants)`, 400);
+    } else {
+      existingStock.reserved -= stockMovement.quantity;
+      await existingStock.save();
+      return 204;
     }
   }
 }
